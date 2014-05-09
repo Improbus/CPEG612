c0eb779c07b5ac20d90f4debe1bda2a63d74d7ba
Improbus <improbus@users.noreply.github.com>
2014-05-09 12:31:55 -0400

    Final Project including EXTJS Code, Android Application, and Final Documents

---
+++ OSMS/Code/OSMS/src/data/Calendars.js
@@ -0,0 +1,18 @@
+Ext.define('Ext.calendar.data.Calendars', {
+    statics: {
+        getData: function(){
+            return {
+                "calendars":[{
+                    "id":    1,
+                    "title": "Baseball"
+                },{
+                    "id":    2,
+                    "title": "Football"
+                },{
+                    "id":    3,
+                    "title": "Basketball"
+                }]
+            };    
+        }
+    }
+});
